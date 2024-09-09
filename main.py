7de9ab7b1068dfc78d82cc8affa4852ebac05aac
Swastik Purathepparambil <swastiksp25@gmail.com>
2024-09-09 10:02:26 -0700

    LookingAtData

---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+
+# csv file name
+filename = "train.csv"
+
+features = []
+rows = []
+
+with open(filename, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    
+    # get all feature names
+    fields = next(csv_reader)
+    for row in csv_reader:
+        rows.append(row)
+    
+    # get total number of rows
+    print("Total no. of rows: %d" % (csv_reader.line_num))
+
+print('Features: ' + ', '.join(field for field in fields))
+
+dataset_size = 500
+X_train = []
+y_train = []
+for row in rows[:dataset_size]:
+    X_train.append(row[:-1])
+    y_train.append(row[-1])
+
+for i in range(25):
+    print(X_train[i])
+    print(y_train[i])
+    print("\n")
